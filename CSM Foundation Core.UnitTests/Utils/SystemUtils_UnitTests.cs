4ed50c1c2a455717ee82df923a25b9fc8493beac
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2025-12-25 01:08:08 -0800

    updated to get global vars

--- CSM Foundation Core.UnitTests/Utils/SystemUtils_UnitTests.cs
+++ CSM Foundation Core.UnitTests/Utils/SystemUtils_UnitTests.cs
@@ -14,7 +14,7 @@ public class SystemUtils_UnitTests {
 
 
     [Fact(DisplayName = "[GetVar(varKey)]: Correctly gets a previous store variable")]
-    public void GetVar_A() {
+    public void GetPreviousStoredVar() {
 
         Environment.SetEnvironmentVariable(_varKey, _varValue);
 
@@ -22,7 +22,7 @@ public class SystemUtils_UnitTests {
     }
 
     [Fact(DisplayName = "[SetVar(varKey, varValue)]: Correctly sets a variable and can be gathered by the system")]
-    public void SetVar_A() {
+    public void SetsVar() {
 
         SystemUtils.SetVar(_varKey, _varValue);
 
@@ -30,7 +30,7 @@ public class SystemUtils_UnitTests {
     }
 
     [Fact(DisplayName = "[GetEnv(varKey)]: Correctly gets the environment (DEVELOPMENT | DEV)")]
-    public void GetEnv_A() {
+    public void GetsVarFromDevelopment() {
         SystemUtils.SetVar(Constants.Framework.ASPNETCORE_ENVIRONEMNT, Constants.Environments.DEVELOPMENT);
 
         SystemEnvs developmentEnv = SystemUtils.GetEnv();
@@ -45,7 +45,7 @@ public class SystemUtils_UnitTests {
     }
 
     [Fact(DisplayName = "[GetEnv(varKey)]: Correctly gets the environment (PRODUCTION | PROD)")]
-    public void GetEnv_B() {
+    public void GetsVarFromProd() {
         SystemUtils.SetVar(Constants.Framework.ASPNETCORE_ENVIRONEMNT, Constants.Environments.PRODUCTION);
 
         SystemEnvs productionEnv = SystemUtils.GetEnv();
@@ -60,7 +60,7 @@ public class SystemUtils_UnitTests {
     }
 
     [Fact(DisplayName = "[GetEnv(varKey)]: Correctly gets the environment (QUALITY | QA)")]
-    public void GetEnv_C() {
+    public void GetsVarFromQuality() {
         SystemUtils.SetVar(Constants.Framework.ASPNETCORE_ENVIRONEMNT, Constants.Environments.QUALITY);
 
         SystemEnvs qualityEnv = SystemUtils.GetEnv();
@@ -75,7 +75,7 @@ public class SystemUtils_UnitTests {
     }
 
     [Fact(DisplayName = "[GetEnv(varKey)]: Correctly gets the environment (LABORATORY | LAB)")]
-    public void GetEnv_D() {
+    public void GetsVarFromLab() {
         SystemUtils.SetVar(Constants.Framework.ASPNETCORE_ENVIRONEMNT, Constants.Environments.LABORATORY);
 
         SystemEnvs laboratoryEnv = SystemUtils.GetEnv();
@@ -87,5 +87,17 @@ public class SystemUtils_UnitTests {
         SystemEnvs labEnv = SystemUtils.GetEnv();
 
         Assert.Equal(SystemEnvs.LAB, laboratoryEnv);
+    }
+
+    [Fact(DisplayName = "[GetGlobalVar(varKey)]: Correctly gets the expected variable value from the target")]
+    public void GetsGlobalVarMapped() {
+        
+        string expectedValue = "USER_VAR";
+
+        // --> First we want to get from User not Process
+        SystemUtils.SetVar(_varKey, expectedValue, EnvironmentVariableTarget.User);
+        SystemUtils.SetVar(_varKey, "PROCESS_VAR", EnvironmentVariableTarget.Process);
+
+        Assert.Equal(expectedValue, SystemUtils.GetGlobalVar(_varKey));
     }
 }
